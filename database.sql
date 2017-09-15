01c567b196972d9c3e145742d4305769789da5d8
Samuel Sutherland <samuelasutherland@gmail.com>
2017-09-15 08:45:25 +0100

    adding not null/unique contraints. RemovingPartyID from Candidates Table

--- database.sql
+++ database.sql
@@ -1,66 +1,64 @@
 CREATE DATABASE IF NOT EXISTS Agora;
-
 use Agora;
 
 CREATE TABLE Users (
-  UserID int not null primary key auto_increment,
-  Username varchar(40),
-	Email varchar(254),
+	UserID int not null primary key auto_increment,
+	Username varchar(40) not null,
+	Email varchar(254) not null,
 	Surname varchar(50),
 	Forename varchar(50),
-  PreferredName varchar(50)
+	PreferredName varchar(50)
 );
 
 CREATE TABLE GroupTypes (
 	GroupTypeID int not null primary key auto_increment,
-	Name varchar(50)
+	Name varchar(50) not null
 );
 
 CREATE TABLE Groups (
 	GroupID int not null primary key auto_increment,
 	Name varchar(50),
-  GroupTypeID int not null,
+	GroupTypeID int not null,
 	FOREIGN KEY (GroupTypeID) REFERENCES GroupTypes(GroupTypeID)
 );
 
 CREATE TABLE LinkGroupsUsers (
 	LinkID int not null primary key auto_increment,
 	GroupID int not null,
-  UserID int not null,
+	UserID int not null,
 	FOREIGN KEY (GroupID) REFERENCES Groups(GroupTypeID),
 	FOREIGN KEY (UserID) REFERENCES Users(UserID)
 );
 
 CREATE TABLE Parties (
 	PartyID int not null primary key auto_increment,
-	Name varchar(100),
+	Name varchar(100) unique not null,
 	PathToLogo varchar(256)
 );
 
 CREATE TABLE Candidates (
 	CandidateID int not null primary key auto_increment,
-	Name varchar(100),
-  PartyID int not null,
+	Name varchar(100) not null,
 	FOREIGN KEY (PartyID) REFERENCES Parties(PartyID)
 );
 
 CREATE TABLE Systems (
 	SystemID int not null primary key auto_increment,
-	Name varchar(50)
+	Name varchar(50) not null unique
 );
 
 CREATE TABLE Elections (
 	ElectionID int not null primary key auto_increment,
-  Name varchar(50)
+	Name varchar(50) unique not null
 );
 
 CREATE TABLE Votes (
 	VoteID int not null primary key auto_increment,
-  Timestamp timestamp,
-  UserID int not null,
-  ElectionID int not null,
-  SystemID int not null,
-  CandidateID int not null,
+	Timestamp timestamp,
+	UserID int not null,
+	ElectionID int not null,
+	SystemID int not null,
+	CandidateID int not null,
 	FOREIGN KEY (UserID) REFERENCES Users(UserID),
 	FOREIGN KEY (ElectionID) REFERENCES Elections(ElectionID),
 	FOREIGN KEY (SystemID) REFERENCES Systems(SystemID),
@@ -71,8 +69,8 @@ CREATE TABLE Votes (
 CREATE TABLE LinkCandidatesElections (
 	LinkID int not null primary key auto_increment,
 	CandidateID int not null,
-  ElectionID int not null,
-  PartyID int not null,
+	ElectionID int not null,
+	PartyID int,
 	FOREIGN KEY (CandidateID) REFERENCES Candidates(CandidateID),
 	FOREIGN KEY (ElectionID) REFERENCES Elections(ElectionID),
 	FOREIGN KEY (PartyID) REFERENCES Parties(PartyID)
@@ -80,8 +78,8 @@ CREATE TABLE LinkCandidatesElections (
 
 CREATE TABLE LinkElectionsSystems (
 	LinkID int not null primary key auto_increment,
-  ElectionID int not null,
-  SystemID int not null,
+	ElectionID int not null,
+	SystemID int not null,
 	FOREIGN KEY (ElectionID) REFERENCES Elections(ElectionID),
 	FOREIGN KEY (SystemID) REFERENCES Systems(SystemID)
 );
