cca9492652464e8ccfbf0332231ecc531ce8a1f9
Samuel Sutherland <samuelasutherland@gmail.com>
2018-02-09 17:39:30 +0000

    Assorted stuff

--- server.js
+++ server.js
@@ -16,21 +16,27 @@ var app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(bodyParser.json({limit: '50mb'}));
+app.use(function(req,res,next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
 
 var PORT = process.env.PORT || 24672;
+var API_STEM_V1 = "/api/v1"
 
-app.get('/', function(req, res){
+app.get(API_STEM_V1+'/', function(req, res){
   res.json({message: 'Hooray! API vaguely works!'});
 });
 
 
-app.get('/users', function(req, res){
+app.get(API_STEM_V1+'/users', function(req, res){
   pool.query('SELECT * FROM USERS', function (err, results, fields){
     if (err) console.log(err);
     res.json(results);
   });
 });
-app.post('/users', function(req, res){
+app.post(API_STEM_V1+'/users', function(req, res){
   pool.query('INSERT INTO USERS SET ?', req.body, function(err, results, fields){
     if (err) {
       console.log(err);
@@ -41,13 +47,13 @@ app.post('/users', function(req, res){
   });
 });
 
-app.get('/users/groups', function(req, res){
+app.get(API_STEM_V1+'/users/groups', function(req, res){
   pool.query('SELECT * FROM GROUPS', function (err, results, fields){
     if (err) console.log(err);
     res.json(results);
   });
 });
-app.post('/users/groups', function(req, res){
+app.post(API_STEM_V1+'/users/groups', function(req, res){
   pool.query('INSERT INTO GROUPS SET ?', req.body, function(err, results, fields){
     if (err) {
       console.log(err);
@@ -58,13 +64,13 @@ app.post('/users/groups', function(req, res){
   });
 });
 
-app.get('/users/groups/types', function(req, res){
+app.get(API_STEM_V1+'/users/groups/types', function(req, res){
   pool.query('SELECT * FROM GROUPTYPES', function (err, results, fields){
     if (err) console.log(err);
     res.json(results);
   });
 });
-app.post('/users/groups/types', function(req, res){
+app.post(API_STEM_V1+'/users/groups/types', function(req, res){
   pool.query('INSERT INTO GROUPTYPES SET ?', req.body, function(err, results, fields){
     if (err) {
       console.log(err);
@@ -75,13 +81,13 @@ app.post('/users/groups/types', function(req, res){
   });
 });
 
-app.get('/users/groups/types/:grouptypeID', function(req, res){
+app.get(API_STEM_V1+'/users/groups/types/:grouptypeID', function(req, res){
   pool.query('SELECT * FROM GROUPTYPES WHERE GROUPTYPEID = ?', req.params.grouptypeID, function (err, results, fields){
     if (err) console.log(err);
     res.json(results[0]);
   });
 });
-app.post('/users/groups/types/:grouptypeID', function(req, res){
+app.post(API_STEM_V1+'/users/groups/types/:grouptypeID', function(req, res){
   pool.query('UPDATE GROUPTYPES SET ? WHERE GROUPTYPEID = ?', [req.body, req.params.grouptypeID], function(err, results, fields){
     if (err) {
       console.log(err);
@@ -91,7 +97,7 @@ app.post('/users/groups/types/:grouptypeID', function(req, res){
     };
   });
 });
-app.delete('/users/groups/types/:grouptypeID', function(req, res){
+app.delete(API_STEM_V1+'/users/groups/types/:grouptypeID', function(req, res){
   console.log(req.body)
   if (req.body['confirm'] == 'delete') {
     pool.query('DELETE FROM GROUPTYPES WHERE GROUPTYPEID = ' + req.params.grouptypeID  + ';', function(err, results, fields){
@@ -107,13 +113,13 @@ app.delete('/users/groups/types/:grouptypeID', function(req, res){
   }
 });
 
-app.get('/users/groups/:groupID', function(req, res){
+app.get(API_STEM_V1+'/users/groups/:groupID', function(req, res){
   pool.query('SELECT * FROM GROUPS WHERE GROUPID = ?', req.params.groupID, function (err, results, fields){
     if (err) console.log(err);
     res.json(results[0]);
   });
 });
-app.post('/users/groups/:groupID', function(req, res){
+app.post(API_STEM_V1+'/users/groups/:groupID', function(req, res){
   pool.query('UPDATE GROUPS SET ? WHERE GROUPID = ?', [req.body, req.params.groupID], function(err, results, fields){
     if (err) {
       console.log(err);
@@ -123,7 +129,7 @@ app.post('/users/groups/:groupID', function(req, res){
     };
   });
 });
-app.delete('/users/groups/:groupID', function(req, res){
+app.delete(API_STEM_V1+'/users/groups/:groupID', function(req, res){
   console.log(req.body)
   if (req.body['confirm'] == 'delete') {
     pool.query('DELETE FROM GROUPS WHERE GROUPID = ' + req.params.groupID  + ';', function(err, results, fields){
@@ -139,13 +145,13 @@ app.delete('/users/groups/:groupID', function(req, res){
   }
 });
 
-app.get('/users/groups/:groupID/users', function(req, res){
+app.get(API_STEM_V1+'/users/groups/:groupID/users', function(req, res){
   pool.query('SELECT * FROM LINKGROUPSUSERS WHERE GROUPID = ?', parseInt(req.params.groupID), function (err, results, fields){
     if (err) console.log(err);
     res.json(results);
   });
 });
-app.post('/users/groups/:groupID/users', function(req, res){
+app.post(API_STEM_V1+'/users/groups/:groupID/users', function(req, res){
   console.log(req.params.groupID  )
   pool.query('INSERT INTO LINKGROUPSUSERS SET GROUPID = ?, ?', [parseInt(req.params.groupID), req.body], function(err, results, fields){
     if (err) {
@@ -156,7 +162,7 @@ app.post('/users/groups/:groupID/users', function(req, res){
     };
   });
 });
-app.delete('/users/groups/:groupID/users/:userID', function(req, res){
+app.delete(API_STEM_V1+'/users/groups/:groupID/users/:userID', function(req, res){
   console.log(req.body)
   if (req.body['confirm'] == 'delete') {
     pool.query('DELETE FROM LINKGROUPSUSERS WHERE USERID = ? AND GROUPID = ?', [parseInt(req.params.userID), parseInt(req.params.groupID)], function(err, results, fields){
@@ -172,13 +178,13 @@ app.delete('/users/groups/:groupID/users/:userID', function(req, res){
   }
 });
 
-app.get('/users/:userID', function(req, res){
+app.get(API_STEM_V1+'/users/:userID', function(req, res){
   pool.query('SELECT * FROM USERS WHERE USERID = ?', parseInt(req.params.userID), function (err, results, fields){
     if (err) console.log(err);
     res.json(results[0]);
   });
 });
-app.post('/users/:userID', function(req, res){
+app.post(API_STEM_V1+'/users/:userID', function(req, res){
   pool.query('UPDATE USERS SET ? WHERE USERID = ?', parseInt(req.params.userID) , req.body, function(err, results, fields){
     if (err) {
       console.log(err);
@@ -188,7 +194,7 @@ app.post('/users/:userID', function(req, res){
     };
   });
 });
-app.delete('/users/:userID', function(req, res){
+app.delete(API_STEM_V1+'/users/:userID', function(req, res){
   console.log(req.body)
   if (req.body['confirm'] == 'delete') {
     pool.query('DELETE FROM USERS WHERE USERID = ?', parseInt(req.params.userID), function(err, results, fields){
@@ -204,7 +210,7 @@ app.delete('/users/:userID', function(req, res){
   }
 });
 
-app.post('/users/:userID/groups', function(req, res){
+app.post(API_STEM_V1+'/users/:userID/groups', function(req, res){
   pool.query('INSERT INTO LINKGROUPSUSERS SET USERID = ?, ?', [parseInt(req.params.userID), req.body], function(err, results, fields){
     if (err) {
       console.log(err);
@@ -214,13 +220,13 @@ app.post('/users/:userID/groups', function(req, res){
     };
   });
 });
-app.get('/users/:userID/groups', function(req, res){
+app.get(API_STEM_V1+'/users/:userID/groups', function(req, res){
   pool.query('SELECT * FROM LINKGROUPSUSERS WHERE USERID = ?', parseInt(req.params.userID), function (err, results, fields){
     if (err) console.log(err);
     res.json(results);
   });
 });
-app.delete('/users/:userID/groups/:groupID', function(req, res){
+app.delete(API_STEM_V1+'/users/:userID/groups/:groupID', function(req, res){
   console.log(req.body)
   if (req.body['confirm'] == 'delete') {
     pool.query('DELETE FROM LINKGROUPSUSERS WHERE USERID = ? AND GROUPID = ?', [parseInt(req.params.userID), parseInt(req.params.groupID)], function(err, results, fields){
@@ -237,13 +243,13 @@ app.delete('/users/:userID/groups/:groupID', function(req, res){
 });
 
 
-app.get('/votes', function(req, res){
+app.get(API_STEM_V1+'/votes', function(req, res){
   pool.query('SELECT * FROM VOTES', function (err, results, fields){
     if (err) console.log(err);
     res.json(results);
   });
 });
-app.post('/votes', function(req, res){
+app.post(API_STEM_V1+'/votes', function(req, res){
   pool.query('INSERT INTO VOTES SET ?', req.body, function(err, results, fields){
     if (err) {
       console.log(err);
@@ -254,13 +260,13 @@ app.post('/votes', function(req, res){
   });
 });
 
-app.get('/votes/:voteID', function(req, res){
+app.get(API_STEM_V1+'/votes/:voteID', function(req, res){
   pool.query('SELECT * FROM VOTES WHERE VOTEID = ?', req.params.voteID, function (err, results, fields){
     if (err) console.log(err);
     res.json(results[0]);
   });
 });
-app.post('/votes/:voteID', function(req, res){
+app.post(API_STEM_V1+'/votes/:voteID', function(req, res){
   pool.query('UPDATE VOTES SET ? WHERE VOTEID = ?', [req.params.voteID, req.body], function(err, results, fields){
     if (err) {
       console.log(err);
@@ -270,7 +276,7 @@ app.post('/votes/:voteID', function(req, res){
     };
   });
 });
-app.delete('/votes/:voteID', function(req, res){
+app.delete(API_STEM_V1+'/votes/:voteID', function(req, res){
   console.log(req.body)
   if (req.body['confirm'] == 'delete') {
     pool.query('DELETE FROM VOTES WHERE VOTEID = ?', req.params.voteID, function(err, results, fields){
@@ -287,13 +293,13 @@ app.delete('/votes/:voteID', function(req, res){
 });
 
 
-app.get('/parties', function(req, res){
+app.get(API_STEM_V1+'/parties', function(req, res){
   pool.query('SELECT * FROM PARTIES', function (err, results, fields){
     if (err) console.log(err);
     res.json(results);
   });
 });
-app.post('/parties', function(req, res){
+app.post(API_STEM_V1+'/parties', function(req, res){
   pool.query('INSERT INTO PARTIES SET ?', req.body, function(err, results, fields){
     if (err) {
       console.log(err);
@@ -304,13 +310,13 @@ app.post('/parties', function(req, res){
   });
 });
 
-app.get('/parties/:partyID', function(req, res){
+app.get(API_STEM_V1+'/parties/:partyID', function(req, res){
   pool.query('SELECT * FROM PARTIES WHERE PARTYID = ?', req.params.partyID, function (err, results, fields){
     if (err) console.log(err);
     res.json(results[0]);
   });
 });
-app.post('/parties/:partyID', function(req, res){
+app.post(API_STEM_V1+'/parties/:partyID', function(req, res){
   pool.query('UPDATE PARTIES SET ? WHERE PARTYID = ?', [req.params.partyID, req.body], function(err, results, fields){
     if (err) {
       console.log(err);
@@ -320,7 +326,7 @@ app.post('/parties/:partyID', function(req, res){
     };
   });
 });
-app.delete('/parties/:partyID', function(req, res){
+app.delete(API_STEM_V1+'/parties/:partyID', function(req, res){
   console.log(req.body)
   if (req.body['confirm'] == 'delete') {
     pool.query('DELETE FROM PARTIES WHERE PARTYID = ?', req.params.partyID, function(err, results, fields){
@@ -337,13 +343,13 @@ app.delete('/parties/:partyID', function(req, res){
 });
 
 
-app.get('/candidates', function(req, res){
+app.get(API_STEM_V1+'/candidates', function(req, res){
   pool.query('SELECT * FROM CANDIDATES', function (err, results, fields){
     if (err) console.log(err);
     res.json(results);
   });
 });
-app.post('/candidates', function(req, res){
+app.post(API_STEM_V1+'/candidates', function(req, res){
   pool.query('INSERT INTO CANDIDATES SET ?', req.body, function(err, results, fields){
     if (err) {
       console.log(err);
@@ -354,13 +360,13 @@ app.post('/candidates', function(req, res){
   });
 });
 
-app.get('/candidates/:candidateID', function(req, res){
+app.get(API_STEM_V1+'/candidates/:candidateID', function(req, res){
   pool.query('SELECT * FROM CANDIDATES WHERE CANDIDATEID = ?', req.params.candidateID, function (err, results, fields){
     if (err) console.log(err);
     res.json(results[0]);
   });
 });
-app.post('/candidates/:candidateID', function(req, res){
+app.post(API_STEM_V1+'/candidates/:candidateID', function(req, res){
   pool.query('UPDATE CANDIDATES SET ? WHERE CANDIDATEID = ?', [req.params.candidateID, req.body], function(err, results, fields){
     if (err) {
       console.log(err);
@@ -370,7 +376,7 @@ app.post('/candidates/:candidateID', function(req, res){
     };
   });
 });
-app.delete('/candidates/:candidateID', function(req, res){
+app.delete(API_STEM_V1+'/candidates/:candidateID', function(req, res){
   console.log(req.body)
   if (req.body['confirm'] == 'delete') {
     pool.query('DELETE FROM CANDIDATES WHERE CANDIDATEID = ?', req.params.candidateID, function(err, results, fields){
@@ -387,13 +393,13 @@ app.delete('/candidates/:candidateID', function(req, res){
 });
 
 
-app.get('/systems', function(req, res){
+app.get(API_STEM_V1+'/systems', function(req, res){
   pool.query('SELECT * FROM SYSTEMS', function (err, results, fields){
     if (err) console.log(err);
     res.json(results);
   });
 });
-app.post('/systems', function(req, res){
+app.post(API_STEM_V1+'/systems', function(req, res){
   pool.query('INSERT INTO SYSTEMS SET ?', req.body, function(err, results, fields){
     if (err) {
       console.log(err);
@@ -404,13 +410,13 @@ app.post('/systems', function(req, res){
   });
 });
 
-app.get('/systems/:systemID', function(req, res){
+app.get(API_STEM_V1+'/systems/:systemID', function(req, res){
   pool.query('SELECT * FROM SYSTEMS WHERE SYSTEMID = ?', req.params.systemID, function (err, results, fields){
     if (err) console.log(err);
     res.json(results[0]);
   });
 });
-app.post('/systems/:systemID', function(req, res){
+app.post(API_STEM_V1+'/systems/:systemID', function(req, res){
   pool.query('UPDATE SYSTEMS SET ? WHERE SYSTEMID = ?', [req.params.systemID, req.body], function(err, results, fields){
     if (err) {
       console.log(err);
@@ -420,7 +426,7 @@ app.post('/systems/:systemID', function(req, res){
     };
   });
 });
-app.delete('/systems/:systemID', function(req, res){
+app.delete(API_STEM_V1+'/systems/:systemID', function(req, res){
   console.log(req.body)
   if (req.body['confirm'] == 'delete') {
     pool.query('DELETE FROM SYSTEMS WHERE SYSTEMID = ?', req.params.systemID, function(err, results, fields){
@@ -437,13 +443,13 @@ app.delete('/systems/:systemID', function(req, res){
 });
 
 
-app.get('/elections', function(req, res){
+app.get(API_STEM_V1+'/elections', function(req, res){
   pool.query('SELECT * FROM ELECTIONS', function (err, results, fields){
     if (err) console.log(err);
     res.json(results);
   });
 });
-app.post('/elections', function(req, res){
+app.post(API_STEM_V1+'/elections', function(req, res){
   pool.query('INSERT INTO ELECTIONS SET ?', req.body, function(err, results, fields){
     if (err) {
       console.log(err);
@@ -454,13 +460,13 @@ app.post('/elections', function(req, res){
   });
 });
 
-app.get('/elections/:electionID', function(req, res){
+app.get(API_STEM_V1+'/elections/:electionID', function(req, res){
   pool.query('SELECT * FROM ELECTIONS WHERE ELECTIONID = ?', req.params.electionID, function (err, results, fields){
     if (err) console.log(err);
     res.json(results[0]);
   });
 });
-app.post('/elections/:electionID', function(req, res){
+app.post(API_STEM_V1+'/elections/:electionID', function(req, res){
   pool.query('UPDATE ELECTIONS SET ? WHERE ELECTIONID = ?', [req.params.electionID, req.body], function(err, results, fields){
     if (err) {
       console.log(err);
@@ -470,7 +476,7 @@ app.post('/elections/:electionID', function(req, res){
     };
   });
 });
-app.delete('/elections/:electionID', function(req, res){
+app.delete(API_STEM_V1+'/elections/:electionID', function(req, res){
   console.log(req.body)
   if (req.body['confirm'] == 'delete') {
     pool.query('DELETE FROM ELECTIONS WHERE ELECTIONID = ?', req.params.electionID, function(err, results, fields){
