8af1d17a85b0737387ff612c626e20437375d67e
Samuel Sutherland <samuelasutherland@gmail.com>
2018-02-17 15:44:47 +0000

    Adding headers to allow headers

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(bodyParser.json({limit: '50mb'}));
 app.use(function(req,res,next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader("Access-Control-Allow-Headers", 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
   next();
 });
 
